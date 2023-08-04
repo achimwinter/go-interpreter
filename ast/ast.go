35c680bb4db2337fb51aaa469805cefda2f5048c
achimwinter <achim.winter95@gmail.com>
2023-08-04 22:13:25 +0200

    add boolean parsing

--- ast/ast.go
+++ ast/ast.go
@@ -64,6 +64,11 @@ type IntegerLiteral struct {
 	Value int64
 }
 
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -159,3 +164,7 @@ func (oe *InfixExpression) String() string {
 func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
