7c26ebcef229763d09c057bba1874b4bb1bacadf
Achim Winter <achim.winter95@gmail.com>
2023-09-29 21:06:11 +0200

    Add environment

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/achimwinter/monkey-language/evaluator"
 	"github.com/achimwinter/monkey-language/lexer"
+	"github.com/achimwinter/monkey-language/object"
 	"github.com/achimwinter/monkey-language/parser"
 )
 
@@ -25,6 +26,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -43,7 +45,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
